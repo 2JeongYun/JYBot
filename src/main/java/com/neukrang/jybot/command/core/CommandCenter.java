f2646ea22aeacc10a6794019080093e62e29f8f1
2JeongYun <leejeongyun828@gmail.com>
2021-11-26 13:21:53 +0900

    디버그: 명령어 이름으로 매핑이 되지 않던 문제 해결

--- src/main/java/com/neukrang/jybot/command/core/CommandCenter.java
+++ src/main/java/com/neukrang/jybot/command/core/CommandCenter.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Component
@@ -17,23 +18,32 @@ public class CommandCenter {
     public void handle(GuildMessageReceivedEvent event) {
         String commandName = extractCommandName(event);
         String commandBeanName = commandName + "Command";
-        if (!commandMap.containsKey(commandBeanName)) {
+
+        Optional<ICommand> command = Optional.ofNullable(commandMap.get(commandBeanName));
+        if (command.isEmpty())
+            command = findByCommandName(commandMap, commandName);
+
+        if (command.isEmpty()) {
             event.getChannel()
                     .sendMessage("해당되는 명령어가 없습니다. '!help' 를 참조하세요.")
                     .queue();
             return;
         }
 
-        ICommand command = commandMap.get(commandBeanName);
-
-        if (!validator.isValid(command.getConstraintList(), event)) {
+        if (!validator.isValid(command.get().getConstraintList(), event)) {
             return;
         }
-        command.handle(event);
+        command.get().handle(event);
     }
 
     private String extractCommandName(GuildMessageReceivedEvent event) {
         String content = event.getMessage().getContentRaw();
         return content.split(" ")[0].replaceFirst("!", "");
+    }
+
+    private Optional<ICommand> findByCommandName(Map<String, ICommand> map, String commandName) {
+        return map.values().stream()
+                .filter(iCommand -> iCommand.getCommandName().equals(commandName))
+                .findFirst();
     }
 }
